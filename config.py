4b4e397fad381d4562576b81e76dac1cf6b87e9b
1850732945 <112037230+1850732945@users.noreply.github.com>
2025-04-04 15:14:52 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,57 +1,14 @@
-# 公众号配置
-# 公众号appId
+# 微信公众号配置
 app_id = "wxf364c54ced4ab1db"
-# 公众号appSecret
 app_secret = "27302f15c5df687c7f844354be670d4c"
-# 模板消息id
-# 每日消息
-template_id1 = "ksWXGq9ccOnOF4NC3wbf9ForaV-1aJ4fWGGM1E6ZdJw"
-# 课程消息,上课提醒
-template_id2 = "loe1yHWr************************************"
-# 晚安心语
-template_id3 = "-fun9-2************************************"
-# 接收公众号消息的微信号
-# 这是openid
-user = ["oTQxd6lTbNtTxTZ2LeIk3rHXBFWU"]
+template_id = "ksWXGq9ccOnOF4NC3wbf9ForaV-1aJ4fWGGM1E6ZdJw"  # 替换为你的模板ID
+user = ["oTQxd6lTbNtTxTZ2LeIk3rHXBFWU"]  # 接收者openid
 
-# 信息配置
-# 所在省份
+# 基础信息配置
 province = "辽宁"
-# 所在城市
 city = "鞍山"
-# 在一起的日子，格式同上------------计时器
-love_date = "2025-4-1"
-# 每日推送时间
-post_Time = "07:20:00"
+love_date = "2025-4-1"  # 认识日期
+post_time = "07:20:00"  # 推送时间
 
-
-# 模板 1：每日提醒模板
-# 本周是开学的第: {{weeks.DATA}} 周
-# 今天是: {{date.DATA}}
-# 城市： {{city.DATA}}
-# 天气： {{weather.DATA}}
-# 最低气温: {{min_temperature.DATA}}
-# 最高气温: {{max_temperature.DATA}}
-# 今天是破壳日的第: {{love_day.DATA}} 天
-# 距离开学还有: {{birthday.DATA}} 天
-# ----------------今日课程----------------
-# 第一讲: {{firstClass.DATA}}
-# 第二讲: {{secondClass.DATA}}
-# 第三讲: {{thirdClass.DATA}}
-# 第四讲: {{fourthClass.DATA}}
-# 第五讲: {{fifthClass.DATA}}
-# 第六讲: {{sixthClass.DATA}}
-
-# 模板 2 课程单推
-# 课程信息: {{classInfo.DATA}}
-
-# 模板 3 晚安心语推送和第二天课程推送
-# {{goodNight.DATA}}
-# ----------------明日课程----------------
-# 明天是: {{week.DATA}}
-# 第一讲: {{firstClass.DATA}}
-# 第二讲: {{secondClass.DATA}}
-# 第三讲: {{thirdClass.DATA}}
-# 第四讲: {{fourthClass.DATA}}
-# 第五讲: {{fifthClass.DATA}}
-# 第六讲: {{sixthClass.DATA}}
+# 天行数据API配置（需申请：https://www.tianapi.com/）
+tianxing_api_key = "你的天行API密钥"  # 替换为实际key
