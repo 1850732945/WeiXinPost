c8cf44ace6e576ba7d12d79b1ea24ca2977809d1
1850732945 <112037230+1850732945@users.noreply.github.com>
2025-04-04 16:52:00 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,14 +2,13 @@
 app_id = "wxf364c54ced4ab1db"
 app_secret = "27302f15c5df687c7f844354be670d4c"
 template_id = "VR9nbE_aecWsMXNsQ-L-dszgymzOXE8-2phN3diL-DM"
-user = ["oTQxd6lTbNtTxTZ2LeIk3rHXBFWU"]
+user = ["oTQxd6lTbNtTxTZ2LearHXBFWU"]
 
 # 基础信息配置
 province = "辽宁"
 city = "鞍山"
-love_date = "2025-4-1"  # 格式必须为YYYY-MM-DD
+love_date = "2025-4-1"
 post_time = "07:20:00"
 
 # 第三方API配置
 tianxing_api_key = "f0d57f5491cee338ba88469762acf2b8"  # 天行数据早安文案
-seniverse_api_key = "PjAZ3gW8ttIMYnYb8"  # 心知天气API密钥（替换为你的真实key）
